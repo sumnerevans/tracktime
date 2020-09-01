d4f577b7415d4b903e477bd598e7bb83527de515
Sumner Evans <me@sumnerevans.com>
2020-09-01 10:32:48 -0600

    Fixed tests and README output

--- tracktime/report.py
+++ tracktime/report.py
@@ -279,8 +279,9 @@ class Report:
             desc = desc + ':'
             lines.append(f'    | {desc.ljust(max_desc_length+2)}{val}')
         lines.append('')
-        lines.append(r'''*\* a week is any set of five days (not necessarily
-            within the same calendar week*''')
+        lines.append(
+            '* a week is any set of five days (not necessarily within the same calendar week)'
+        )
         lines.append('')
 
         # Include the report table
