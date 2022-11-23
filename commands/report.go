9214c5b67e5a005c9383c9e0500d4c4a1b846636
Sumner Evans <me@sumnerevans.com>
2022-11-23 01:30:55 -0700

    report: extract to separate command file
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

---
+++ commands/report.go
@@ -0,0 +1,66 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/sumnerevans/tracktime/lib"
+)
+
+type Sort int
+
+const (
+	SortAlphabetical Sort = iota
+	SortTimeSpent
+)
+
+func (s *Sort) UnmarshalText(b []byte) error {
+	switch string(b) {
+	case "alphabetical", "alpha", "a":
+		*s = SortAlphabetical
+	case "time-spent", "time", "t":
+		*s = SortTimeSpent
+	default:
+		return fmt.Errorf("invalid sort value '%s'", string(b))
+	}
+	return nil
+}
+
+type Report struct {
+	// Specify the range positionally
+	Start string `arg:"positional" help:"specify the start of the reporting range (defaults to the beginning of last month)"`
+	End   string `arg:"positional" help:"specify the end of the reporting range (defaults to the end of last month)"`
+
+	// Specify the range using shorthand
+	Month     string `arg:"-m,--month" help:"shorthand for reporting over an entire month (can be combined with --year, accepted formats: 01, 1, Jan, January, 2019-01)"`
+	Year      int    `arg:"-y,--year" help:"shorthand for reporting over an entire year (can be combined with --month)"`
+	Today     bool   `arg:"--today" help:"shorthand for reporting on today"`
+	Yesterday bool   `arg:"--yesterday" help:"shorthand for reporting on yesterday"`
+	ThisWeek  bool   `arg:"--thisweek" help:"shorthand for reporting on the current week (Sunday-today)"`
+	LastWeek  bool   `arg:"--lastweek" help:"shorthand for reporting on last week (Sunday-Saturday)"`
+	ThisMonth bool   `arg:"--thismonth" help:"shorthand for reporting on the current month"`
+	LastMonth bool   `arg:"--lastmonth" help:"shorthand for reporting on last month"`
+	ThisYear  bool   `arg:"--thisyear" help:"shorthand for reporting on the current year"`
+	LastYear  bool   `arg:"--lastyear" help:"shorthand for reporting on last year"`
+
+	// Specify the grains to show
+	TaskGrain          bool `arg:"--taskgrain" help:"report on the task grain"`
+	NoTaskGrain        bool `arg:"--no-taskgrain" help:"do not report on the task grain"`
+	DescriptionGrain   bool `arg:"--no-descriptiongrain" help:"report on the task grain"`
+	NoDescriptionGrain bool `arg:"--no-descriptiongrain" help:"do not report on the task grain"`
+
+	// Narrow the set of time entries to report on
+	Customer string `arg:"-c,--customer" help:"customer ID to generate a report for"`
+	Project  string `arg:"-p,--project" help:"project name to generate a report for"`
+
+	// How to sort the report
+	Sort Sort `arg:"-s,--sort" help:"the grain to sort the report by (alphabetical,alpha,a or time-spent,time,t)" default:"alphabetical"`
+	Desc bool `arg:"--desc" help:"sort descending"`
+	Asc  bool `arg:"--asc" help:"sort ascending"`
+
+	// Output file
+	OutputFile string `arg:"-o,--outfile" help:"specify the filename to export the report to (supports PDF, HTML, and RST files, if set to '-' then the report is printed to stdout)" default:"-"`
+}
+
+func (s *Report) Run(config *lib.Config) error {
+	return nil
+}
--- tracktime.go
+++ tracktime.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	arg "github.com/alexflint/go-arg"
@@ -45,74 +44,15 @@ func (s *sync) Run(config *lib.Config) error {
 	return nil
 }
 
-type Sort int
-
-const (
-	SortAlphabetical Sort = iota
-	SortTimeSpent
-)
-
-func (s *Sort) UnmarshalText(b []byte) error {
-	switch string(b) {
-	case "alphabetical", "alpha", "a":
-		*s = SortAlphabetical
-	case "time-spent", "time", "t":
-		*s = SortTimeSpent
-	default:
-		return fmt.Errorf("invalid sort value '%s'", string(b))
-	}
-	return nil
-}
-
-type report struct {
-	// Specify the range positionally
-	Start string `arg:"positional" help:"specify the start of the reporting range (defaults to the beginning of last month)"`
-	End   string `arg:"positional" help:"specify the end of the reporting range (defaults to the end of last month)"`
-
-	// Specify the range using shorthand
-	Month     string `arg:"-m,--month" help:"shorthand for reporting over an entire month (can be combined with --year, accepted formats: 01, 1, Jan, January, 2019-01)"`
-	Year      int    `arg:"-y,--year" help:"shorthand for reporting over an entire year (can be combined with --month)"`
-	Today     bool   `arg:"--today" help:"shorthand for reporting on today"`
-	Yesterday bool   `arg:"--yesterday" help:"shorthand for reporting on yesterday"`
-	ThisWeek  bool   `arg:"--thisweek" help:"shorthand for reporting on the current week (Sunday-today)"`
-	LastWeek  bool   `arg:"--lastweek" help:"shorthand for reporting on last week (Sunday-Saturday)"`
-	ThisMonth bool   `arg:"--thismonth" help:"shorthand for reporting on the current month"`
-	LastMonth bool   `arg:"--lastmonth" help:"shorthand for reporting on last month"`
-	ThisYear  bool   `arg:"--thisyear" help:"shorthand for reporting on the current year"`
-	LastYear  bool   `arg:"--lastyear" help:"shorthand for reporting on last year"`
-
-	// Specify the grains to show
-	TaskGrain          bool `arg:"--taskgrain" help:"report on the task grain"`
-	NoTaskGrain        bool `arg:"--no-taskgrain" help:"do not report on the task grain"`
-	DescriptionGrain   bool `arg:"--no-descriptiongrain" help:"report on the task grain"`
-	NoDescriptionGrain bool `arg:"--no-descriptiongrain" help:"do not report on the task grain"`
-
-	// Narrow the set of time entries to report on
-	Customer string `arg:"-c,--customer" help:"customer ID to generate a report for"`
-	Project  string `arg:"-p,--project" help:"project name to generate a report for"`
-
-	// How to sort the report
-	Sort Sort `arg:"-s,--sort" help:"the grain to sort the report by (alphabetical,alpha,a or time-spent,time,t)" default:"alphabetical"`
-	Desc bool `arg:"--desc" help:"sort descending"`
-	Asc  bool `arg:"--asc" help:"sort ascending"`
-
-	// Output file
-	OutputFile string `arg:"-o,--outfile" help:"specify the filename to export the report to (supports PDF, HTML, and RST files, if set to '-' then the report is printed to stdout)" default:"-"`
-}
-
-func (s *report) Run(config *lib.Config) error {
-	return nil
-}
-
 type args struct {
-	Start      *commands.Start `arg:"subcommand" help:"start a new time entry for today"`
-	Stop       *stop           `arg:"subcommand" help:"stop the current time entry"`
-	Resume     *resume         `arg:"subcommand" help:"resume a time entry from today"`
-	List       *commands.List  `arg:"subcommand" help:"list the time entries for a date"`
-	Edit       *edit           `arg:"subcommand" help:"edit time entries for a date"`
-	Sync       *sync           `arg:"subcommand" help:"synchronize time spent on tasks for a month to external services"`
-	Report     *report         `arg:"subcommand" help:"output a report about time spent in a time range"`
-	ConfigFile lib.Filename    `arg:"--config" help:"the configuration file to use" default:"$HOME/.config/tracktime/tracktimerc"`
+	Start      *commands.Start  `arg:"subcommand" help:"start a new time entry for today"`
+	Stop       *stop            `arg:"subcommand" help:"stop the current time entry"`
+	Resume     *resume          `arg:"subcommand" help:"resume a time entry from today"`
+	List       *commands.List   `arg:"subcommand" help:"list the time entries for a date"`
+	Edit       *edit            `arg:"subcommand" help:"edit time entries for a date"`
+	Sync       *sync            `arg:"subcommand" help:"synchronize time spent on tasks for a month to external services"`
+	Report     *commands.Report `arg:"subcommand" help:"output a report about time spent in a time range"`
+	ConfigFile lib.Filename     `arg:"--config" help:"the configuration file to use" default:"$HOME/.config/tracktime/tracktimerc"`
 }
 
 func (args) Version() string {
